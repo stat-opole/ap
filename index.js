b6658a7698678aea4bad8e6f4826b0ded477fecc
stat-opole <66861258+stat-opole@users.noreply.github.com>
2020-06-20 21:50:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,65 @@
 Survey
     .StylesManager
-    .applyTheme("orange");
+    .applyTheme("modern");
 
 var json = {
-    "title": "Survey Title&Logo demo",
-    "description": "Please take look at the survey title and logo. Test the settings on the right panel ->",
-    "logo": "https://surveyjs.io/favicon.ico",
-    "logoWidth": 60,
-    "logoHeight": 60,
-    "questions": [
+    title: "American History",
+    showProgressBar: "bottom",
+    showTimerPanel: "top",
+    maxTimeToFinishPage: 10,
+    maxTimeToFinish: 25,
+    firstPageIsStarted: true,
+    startSurveyText: "Start Quiz",
+    pages: [
         {
-            "name": "name",
-            "type": "text",
-            "title": "Please enter your name:",
-            "placeHolder": "Jon Snow",
-            "isRequired": true
-        }, {
-            "name": "birthdate",
-            "type": "text",
-            "inputType": "date",
-            "title": "Your birthdate:",
-            "isRequired": true
-        }, {
-            "name": "color",
-            "type": "text",
-            "inputType": "color",
-            "title": "Your favorite color:"
-        }, {
-            "name": "email",
-            "type": "text",
-            "inputType": "email",
-            "title": "Your e-mail:",
-            "placeHolder": "jon.snow@nightwatch.org",
-            "isRequired": true,
-            "validators": [
+            questions: [
                 {
-                    "type": "email"
+                    type: "html",
+                    html: "You are about to start quiz by history. <br/>You have 10 seconds for every page and 25 seconds for the whole survey of 3 questions.<br/>Please click on <b>'Start Quiz'</b> button when you are ready."
+                }
+            ]
+        }, {
+            questions: [
+                {
+                    type: "radiogroup",
+                    name: "civilwar",
+                    title: "When was the Civil War?",
+                    choices: [
+                        "1750-1800", "1800-1850", "1850-1900", "1900-1950", "after 1950"
+                    ],
+                    correctAnswer: "1850-1900"
+                }
+            ]
+        }, {
+            questions: [
+                {
+                    type: "radiogroup",
+                    name: "libertyordeath",
+                    title: "Who said 'Give me liberty or give me death?'",
+                    choicesOrder: "random",
+                    choices: [
+                        "John Hancock", "James Madison", "Patrick Henry", "Samuel Adams"
+                    ],
+                    correctAnswer: "Patrick Henry"
+                }
+            ]
+        }, {
+            maxTimeToFinish: 15,
+            questions: [
+                {
+                    type: "radiogroup",
+                    name: "magnacarta",
+                    title: "What is the Magna Carta?",
+                    choicesOrder: "random",
+                    choices: [
+                        "The foundation of the British parliamentary system", "The Great Seal of the monarchs of England", "The French Declaration of the Rights of Man", "The charter signed by the Pilgrims on the Mayflower"
+                    ],
+                    correctAnswer: "The foundation of the British parliamentary system"
                 }
             ]
         }
-    ]
+    ],
+    completedHtml: "<h4>You have answered correctly <b>{correctedAnswers}</b> questions from <b>{questionCount}</b>.</h4>"
 };
 
 window.survey = new Survey.Model(json);
