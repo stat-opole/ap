7d4b7912892e1c6c34bb1cf08c0b935b9abfdea3
stat-opole <66861258+stat-opole@users.noreply.github.com>
2020-06-30 15:08:52 +0200

    Delete java.js

--- java.js
+++
@@ -1,5 +0,0 @@
-﻿
-document.addEventListener("DOMContentLoaded", function() {
-    const startBtn = document.querySelector(".game-start");
-    startBtn.addEventListener("click", () => alert("Hello! aaaaaaaaaaaaa!"));
-});
